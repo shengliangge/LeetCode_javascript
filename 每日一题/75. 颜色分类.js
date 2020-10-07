3ee1c20167eb8e43def18b8838fb47c6a5d03dd4
shengliangge <850110183@qq.com>
2020-10-07 23:02:27 +0800

    75

---
+++ 每日一题/75. 颜色分类.js
@@ -0,0 +1,25 @@
+// 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+// 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+// 注意:
+// 不能使用代码库中的排序函数来解决这道题。
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  for (let i = 0; i < nums.length-1; i++) {
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let temp = nums[j]
+        nums[j] = nums[j + 1]
+        nums[j + 1] = temp
+      }
+    }
+  }
+  console.log(nums)
+};
+
+sortColors([2,0,2,1,1,0])
