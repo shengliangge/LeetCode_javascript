a5a8f2d6eaac8a216b3ecf8819f306c8246b2c65
shengliangge <850110183@qq.com>
2020-11-28 10:21:42 +0800

    提交

---
+++ 191.位1的个数.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let str =n.toString(2), count = 0
+  for (let i = 0; i < str.length; i++) {
+      console.log(str[i])
+      if (str[i] == '1') count++
+  }
+  return count
+};
